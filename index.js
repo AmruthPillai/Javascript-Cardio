3e454ed3d55a07ea78b068aed84dd231f97981cf
AmruthPillai <im.amruth@gmail.com>
2018-02-25 22:21:29 +0530

    isPalindrome

--- index.js
+++ index.js
@@ -47,7 +47,10 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+    const revString = str.split('').reverse().join('');
+    return revString === str;
+}
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
@@ -70,6 +73,6 @@ function maxCharacter(str) {}
 function fizzBuzz() {}
 
 // Call Function
-const output = reverseString('hello');
+const output = isPalindrome('madam');
 
 console.log(output);
