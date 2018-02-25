76452843f7e2831a0d9d07190cb3378d078837a3
AmruthPillai <im.amruth@gmail.com>
2018-02-25 22:41:20 +0530

    capitalizeLetters

--- index.js
+++ index.js
@@ -64,7 +64,26 @@ function reverseInt(int) {
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+    // Method 1 -----
+    // const strArr = str.toLowerCase().split(' ');
+    // for (let i = 0; i < strArr.length; i++) {
+    //     strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+    // }
+    // return strArr.join(' ');
+
+    // Method 2 -----
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(word => word[0].toUpperCase() + word.substr(1))
+        .join(' ');
+
+    // Method 3 -----
+    // return str.replace(/\b[a-z]/gi, char => {
+    //     return char.toUpperCase();
+    // });
+}
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
@@ -76,6 +95,6 @@ function maxCharacter(str) {}
 function fizzBuzz() {}
 
 // Call Function
-const output = reverseInt(-12345);
+const output = capitalizeLetters('i love javascript');
 
 console.log(output);
